4b0a58cd5b1b30a5b994eaf609a144a071a30992
Yerko Asbun <asbunyerko01@gmail.com>
2024-09-11 01:51:41 -0400

    final correction

--- src/controllers/country.controllers.js
+++ src/controllers/country.controllers.js
@@ -1,24 +1,20 @@
 const catchError = require('../utils/catchError');
 const axios = require('axios');
 
-const getAll = catchError(async (req, res) => {
-    try {
-        const response = await axios.get('https://date.nager.at/api/v3/AvailableCountries');
-        return res.json(response.data);
-    } catch (error) {
-        console.error('Error fetching countries:', error);
-        return res.status(500).json({ message: 'An error occurred while fetching countries' });
-    }
-});
 
-const getInfo = catchError(async (req, res) => {
-    const { data1, data2, data3 } = req.params;
-    
-    try {
-        let url = `https://countriesnow.space/api/v0.1/${data1}/${data2}`;
-        if (data3) {
-            url += `/${data3}`;
+    const getAll = catchError(async (req, res) => {
+    const { data1, data2 } = req.params;
+
+    if (data1 !== 'population' && data1 !== 'flag') {
+
+    let url = `https://date.nager.at/api/v3/${data1}`;
+
+        if (data2) {
+            url += `/${data2}`;
         }
+
+    try {
+        
 
         const response = await axios.get(url);
         return res.json(response.data);
@@ -26,7 +22,45 @@ const getInfo = catchError(async (req, res) => {
         console.error(`Error fetching data from ${url}:`, error);
         return res.status(500).json({ message: 'An error occurred while fetching data' });
     }
-});
+}else{
+    
+    let url = `https://countriesnow.space/api/v0.1/countries/${data1}`;
+    if (data2) {
+        url += `/${data2}`;
+    }
+
+try {
+    
+
+    const response = await axios.get(url);
+    return res.json(response.data);
+} catch (error) {
+    console.error(`Error fetching data from ${url}:`, error);
+    return res.status(500).json({ message: 'An error occurred while fetching data' });
+}
+
+}}); 
+
+const getInfo = catchError(async (req, res) => {
+    const { data1, data2 } = req.params;
+
+    if (data1 === 'population' || 'flag') {
+
+    let url = `https://countriesnow.space/api/v0.1/countries/${data1}`;
+        if (data2) {
+            url += `/${data2}`;
+        }
+    
+    try {
+        
+
+        const response = await axios.get(url);
+        return res.json(response.data);
+    } catch (error) {
+        console.error(`Error fetching data from ${url}:`, error);
+        return res.status(500).json({ message: 'An error occurred while fetching data' });
+    }
+}});
 
 
 module.exports = {
--- src/routes/country.router.js
+++ src/routes/country.router.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const routerCountry = express.Router();
 
-routerCountry.route('/')
+routerCountry.route('/:data1/:data2?')
     .get(getAll)
 
-routerCountry.route('/:data1/:data2/:data3?')
+routerCountry.route('/:data1/:data2?')
     .get(getInfo)
     
 
